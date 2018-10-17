05b4647e178546a4763a5b567dd4adea6af1b696
Nekmo <contacto@nekmo.com>
2018-10-17 19:47:58 +0200

    Issue #3: Optional init_reader for tests (TestProvider).

--- tests/test_providers.py
+++ tests/test_providers.py
@@ -5,9 +5,13 @@
 import unittest
 from ._compat import Mock, patch
 
-from proxy_db.providers import ProxyNovaCom
+from proxy_db.providers import ProxyNovaCom, Provider
 
-
+URL = 'https://domain.com/'
+PROVIDER_HTML = """
+Proxy: 12.131.91.51:8888
+<b>Other: 8.10.81.82:7171.</b> 
+"""
 PROXY_NOVA_HTML = """
     <tr data-proxy-id="00000000">
         <td align="left" onclick="javascript:check_proxy(this)">
@@ -26,6 +30,29 @@ PROXY_NOVA_HTML = """
         </td>
     </tr>
 """
+
+
+class TestProvider(unittest.TestCase):
+    url = URL
+    proxies = [{'proxy': ('12.131.91.51', '8888')}, {'proxy': ('8.10.81.82', '7171')}]
+
+    def test_request(self):
+        country = 'es'
+        provider_request = Provider(self.url).request(self.url, country)
+        self.assertEqual(provider_request.url, self.url)
+        self.assertEqual(provider_request.options, {'country': country})
+
+    def test_find_page_proxies(self):
+        request = Mock()
+        request.text = PROVIDER_HTML
+        proxies = Provider(self.url).find_page_proxies(request)
+        self.assertEqual(proxies, self.proxies)
+
+    @patch('proxy_db.providers.create_session')
+    def test_process_proxies(self, mock_session):
+        Provider(self.url).process_proxies(self.proxies[:1])
+        mock_session.assert_called_once()
+        mock_session.return_value.commit.assert_called_once()
 
 
 class TestProxyNovaCom(unittest.TestCase):
