7ceee27b2c6055ee1421f23a3d71df769cf29d9f
Nekmo <contacto@nekmo.com>
2018-10-21 03:51:57 +0200

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -1,69 +1,142 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-"""The setup script."""
-
-PACKAGE = 'proxy_db'
-
+"""Manage free and private proxies on local db for Python Projects.
+"""
+import os
+import glob
+from itertools import chain
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
-    readme = readme_file.read()
+AUTHOR = "Nekmo"
+EMAIL = 'contacto@nekmo.com'
+URL = 'https://github.com/Nekmo/proxy-db/'
 
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
+PACKAGE_NAME = 'proxy-db'
+PACKAGE_DOWNLOAD_URL = 'https://github.com/Nekmo/proxy-db/archive/master.zip'
+MODULE = 'proxy_db'
+REQUIREMENT_FILE = 'requirements.txt'
+STATUS_LEVEL = 5  # 1:Planning 2:Pre-Alpha 3:Alpha 4:Beta 5:Production/Stable 6:Mature 7:Inactive
+KEYWORDS = ['proxy-db']
+LICENSE = 'MIT license'
 
-requirements = [
-    'Click>=6.0',
-    # TODO: put package requirements here
+CLASSIFIERS = [  # https://github.com/github/choosealicense.com/tree/gh-pages/_licenses
+    'License :: OSI Approved :: MIT License',
+    # 'License :: OSI Approved :: BSD License',
+    # 'License :: OSI Approved :: ISC License (ISCL)',
+    # 'License :: OSI Approved :: Apache Software License',
+    # 'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+]  # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+NATURAL_LANGUAGE = 'English'
+
+PLATFORMS = [
+    # 'universal',
+    'linux',
+    # 'macosx',
+    # 'solaris',
+    # 'irix',
+    # 'win'
+    # 'bsd'
+    # 'ios'
+    # 'android'
 ]
+PYTHON_VERSIONS = ['2.7', '3.4-3.7']
 
-setup_requirements = [
-    'pytest-runner',
-    # TODO(Nekmo): put setup requirements (distutils extensions, etc.) here
-]
 
-test_requirements = [
-    'pytest',
-    # TODO: put package test requirements here
-]
+def read_requirement_file(path):
+    with open(path) as f:
+        return f.readlines()
 
-package_version = __import__(PACKAGE).__version__
+
+def get_package_version(module_name):
+    return __import__(module_name).__version__
+
+
+def get_packages(directory):
+    # Search modules and submodules to install (module, module.submodule, module.submodule2...)
+    packages_list = find_packages(directory)
+    # Prevent include symbolic links
+    for package in tuple(packages_list):
+        path = os.path.join(directory, package.replace('.', '/'))
+        if not os.path.exists(path) or os.path.islink(path):
+            packages_list.remove(package)
+    return packages_list
+
+
+def get_python_versions(string_range):
+    if '-' not in string_range:
+        return [string_range]
+    return ['{0:.1f}'.format(version * 0.1) for version
+            in range(*[int(x * 10) + (1 * i) for i, x in enumerate(map(float, string_range.split('-')))])]
+
+
+def get_python_classifiers(versions):
+    for version in range(2, 4):
+        if not next(iter(filter(lambda x: int(float(x)) != version, versions.copy())), False):
+            versions.add('{} :: Only'.format(version))
+            break
+    return ['Programming Language :: Python :: %s' % version for version in versions]
+
+
+def get_platform_classifiers(platform):
+    parts = {
+        'linux': ('POSIX', 'Linux'),
+        'win': ('Microsoft', 'Windows'),
+        'solaris': ('POSIX', 'SunOS/Solaris'),
+        'aix': ('POSIX', 'Linux'),
+        'unix': ('Unix',),
+        'bsd': ('POSIX', 'BSD')
+    }[platform]
+    return ['Operating System :: {}'.format(' :: '.join(parts[:i+1]))
+            for i in range(len(parts))]
+
+
+# paths
+here = os.path.abspath(os.path.dirname(__file__))
+readme = glob.glob('{}/{}*'.format(here, 'README'))[0]
+scripts = [os.path.join('scripts', os.path.basename(script))
+           for script in glob.glob('{}/scripts/*'.format(here))]
+
+# Package data
+packages = get_packages(here)
+modules = list(filter(lambda x: '.' not in x, packages))
+module = MODULE if MODULE else modules[0]
+python_versions = set(chain(*[get_python_versions(versions) for versions in PYTHON_VERSIONS])) - {2.8, 2.9}
+status_name = ['Planning', 'Pre-Alpha', 'Alpha', 'Beta',
+               'Production/Stable', 'Mature', 'Inactive'][STATUS_LEVEL - 1]
+
+# Classifiers
+classifiers = CLASSIFIERS.copy()
+classifiers.extend(get_python_classifiers(python_versions))
+classifiers.extend(chain(*[get_platform_classifiers(platform) for platform in PLATFORMS]))
+classifiers.extend([
+    'Natural Language :: {}'.format(NATURAL_LANGUAGE),
+    'Development Status :: {} - {}'.format(STATUS_LEVEL, status_name),
+])
 
 
 setup(
-    name='proxy-db',
-    version=package_version,
-    description="Manage free and private proxies on local db for Python Projects.",
-    long_description=readme + '\n\n' + history,
-    author="Nekmo Com",
-    author_email='contacto@nekmo.com',
-    url='https://github.com/Nekmo/proxy-db',
-    packages=find_packages(include=['proxy_db']),
-    entry_points={
-        'console_scripts': [
-            'proxy_db=proxy_db.cli:main'
-        ]
-    },
+    name=PACKAGE_NAME,
+    version=get_package_version(module),
+    packages=packages,
+    provides=modules,
+    scripts=scripts,
     include_package_data=True,
-    install_requires=requirements,
-    license="Apache Software License 2.0",
+
+    description=__doc__,
+    long_description=open(readme, 'r').read(),
+    keywords=KEYWORDS,
+    download_url=PACKAGE_DOWNLOAD_URL,
+
+    author=AUTHOR,
+    author_email=EMAIL,
+    url=URL,
+
+    classifiers=classifiers,
+    platforms=PLATFORMS,
+
+    install_requires=read_requirement_file(REQUIREMENT_FILE),
+
+    # entry_points={},
+
     zip_safe=False,
-    keywords='proxy_db',
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-    ],
-    test_suite='tests',
-    tests_require=test_requirements,
-    setup_requires=setup_requirements,
 )
