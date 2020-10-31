d2cae0484ade087219f653d7f0fcc9405949a867
Nekmo <contacto@nekmo.com>
2020-10-31 02:15:04 +0100

    TestIpCountry.

--- tests/test_countries.py
+++ tests/test_countries.py
@@ -1,5 +1,7 @@
 import unittest
 
+from geoip2.errors import AddressNotFoundError
+
 from proxy_db.countries import ip_country
 from ._compat import patch
 
@@ -9,3 +11,13 @@ class TestIpCountry(unittest.TestCase):
     def test_ip_country(self, m):
         ip_country('proxy')
         m.__getitem__.assert_called_once()
+
+    @patch('proxy_db.countries.geoip2_manager.is_license_key_available', return_value=False)
+    def test_without_license(self, m):
+        self.assertEqual(ip_country('proxy'), '')
+        m.__getitem__.assert_not_called()
+
+    @patch('proxy_db.countries.geoip2_manager')
+    def test_address_not_found(self, m):
+        m.__getitem__.side_effect = AddressNotFoundError
+        self.assertEqual(ip_country('proxy'), '')
