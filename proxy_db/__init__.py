c8a74daf85f7cfa089b72fb43182447fa3d36627
Nekmo <contacto@nekmo.com>
2020-11-07 01:12:28 +0100

    Bump version: 0.2.5 → 0.3.0

--- proxy_db/__init__.py
+++ proxy_db/__init__.py
@@ -5,7 +5,7 @@ import logging
 
 __author__ = """Nekmo Com"""
 __email__ = 'contacto@nekmo.com'
-__version__ = '0.2.5'
+__version__ = '0.3.0'
 
 logging.basicConfig()
 logging.getLogger('sqlalchemy').setLevel(logging.ERROR)
