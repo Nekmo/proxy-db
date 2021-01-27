c3a77f3606fc702ffd6938d055b0f9963212d393
Nekmo <contacto@nekmo.com>
2021-01-27 23:38:25 +0100

    Issue #29: Add proxies manually

---
+++ proxy_db/management.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+"""Console script for proxy_db."""
+
+import click
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+def add():
+    pass
+
+
+if __name__ == '__main__':
+    cli()
