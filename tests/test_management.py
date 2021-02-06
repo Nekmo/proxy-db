42a4b2a7ce304f2cd2f2e66fc86364fa61695804
Nekmo <contacto@nekmo.com>
2021-02-06 02:50:44 +0100

    Issue #29: Add proxies manually

--- tests/test_management.py
+++ tests/test_management.py
@@ -13,3 +13,13 @@ class TestAdd(unittest.TestCase):
         CliRunner().invoke(add, ['http://1.2.3.4:999'])
         m.assert_called_once_with('manual')
         m.return_value.add_proxies.assert_called_once_with([{'protocol': 'http', 'proxy': '1.2.3.4:999'}], 10)
+
+    @patch('proxy_db.management.click.get_text_stream', return_value=[])
+    def test_get_text_stream(self, m):
+        CliRunner().invoke(add, [])
+        m.assert_called_once()
+
+    @patch('proxy_db.management.click.echo', return_value=[])
+    def test_invalid_proxy(self, m):
+        CliRunner().invoke(add, ['invalid-proxy'])
+        self.assertTrue(m.call_args_list[0][1]['err'])  #  click.echo('...', err=True)
